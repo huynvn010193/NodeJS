d88bac9fd5ade654475e77cd842bfda306b5913b
huynvn010193 <hpn010193@gmail.com>
2019-02-09 22:06:38 +0700

    Done clip: Xây dựng trang NodeJS đầu tiên-02:Module

--- Lesson01/app.js
+++ Lesson01/app.js
@@ -1,13 +1,17 @@
 const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    res.end('Hello NodeJS\n');
+const moduleOne = require('./libs');
+
+http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type','text/plain');
+	res.end('Hello NodeJS\n');
+}).listen(moduleOne.port,moduleOne.hostname, () => {
+	console.log(`Server running at http://${moduleOne.hostname}:${moduleOne.port}/`);
+	moduleOne.show();
 });
 
-server.listen(port,hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);   
-});
+// server.listen(moduleOne.port,moduleOne.hostname, () => {
+// 	console.log(`Server running at http://${moduleOne.hostname}:${moduleOne.port}/`);
+// 	moduleOne.show();
+// });
---
+++ Lesson01/libs.js
@@ -0,0 +1,17 @@
+// function showNotify(){
+// 	console.log('Hello Moudle');
+// }
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// module.exports.show = showNotify;
+// module.exports.hostname = hostname;
+// module.exports.port = port;
+
+module.exports = {
+  show: function (){
+    console.log('Hello Moudle');
+  },
+  hostname: '127.0.0.1',
+  port: 6969
+}
