8676e3951ad524aa2b2c272d51e176d7433038ef
huynvn010193 <hpn010193@gmail.com>
2019-04-05 21:43:36 +0700

    Done clip Done clip 2.4:Setup router and Prefix Admin: Setup Router02

--- Lesson02/routes/frontend/home.js
+++ Lesson02/routes/frontend/home.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { pageTitle: 'AdminPage' });
+  res.render('page/publish/index', { pageTitle: 'Publish' });
 });
 
 module.exports = router;
