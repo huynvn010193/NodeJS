8fc57e52a84ec45594d7a7c5af7f996f37a15d74
huynvn010193 <hpn010193@gmail.com>
2019-05-25 23:50:59 +0700

    Done clip 14

--- project-movie-api/index.js
+++ project-movie-api/index.js
@@ -4,15 +4,28 @@ let app = express();
 let MovieStore = require('./moviestore');
 let movieStore = new MovieStore();
 
-let indexHandler = (req,res) => {
+app.get('/movies',(req,res) => {
 	return res.send(movieStore.all());
-}
+});
 
-app.get('/movies',indexHandler);
+// redirect trang index
+app.get('/', (req,res) => {
+	return res.redirect('/movies');
+});
 
-app.get('/bye',(req,res) => {
-	return res.send('bye bye, see you again');
-})
+app.get('/movies/:title', (req,res) => {
+	let foundMovies = movieStore.find(req.params.title);
+	if(foundMovies.length < 1) {
+		res.statusCode = 404;
+		return res.send({
+			message: 'movie not found'
+		})
+	}
+	return res.send({
+		message: 'found movie',
+		payload: foundMovies.pop()
+	});
+});
 
 app.listen(8080,() => {
 	console.log('server started at: 127.0.0.1:8080');
--- project-movie-api/moviestore.js
+++ project-movie-api/moviestore.js
@@ -7,6 +7,10 @@ class MovieStore{
 	all(){
 		return this.movieData;
 	}
+
+	find(title){
+		return this.movieData.filter(m => m.Title === title);
+	}
 }
 
 module.exports = MovieStore;
