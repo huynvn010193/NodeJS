857f74c1646549aee03919e06fa63b7a5f480e1b
huynvn010193 <hpn010193@gmail.com>
2019-06-09 14:59:17 +0700

    ZendVN-3.2: Count by status: Use .count of mongoose count number status in database

--- Lesson02/routes/backend/items.js
+++ Lesson02/routes/backend/items.js
@@ -5,10 +5,18 @@ const ItemModel = require('../../schemas/items');
 
 router.get('/', (req, res, next) => {
   let statusFilter = [
-    {name: 'All', count: 1, link:'#', class: 'default'},
-    {name: 'Active', count: 2, link:'#', class: 'success'},
-    {name: 'InActive', count: 3, link:'#', class: 'warning'}
+    {name: 'All', value:'all', count: 1, link:'#', class: 'default'},
+    {name: 'Active', value:'active', count: 2, link:'#', class: 'success'},
+    {name: 'InActive', value:'inactive', count: 3, link:'#', class: 'warning'}
   ];
+
+  statusFilter.forEach((item,index) => {
+    let condition = item.value !== "all" ? {status: item.value}:{};
+    ItemModel.count(condition).then((data) => {
+      statusFilter[index].count = data;
+    });
+  })
+  
   ItemModel.find({ }).then((items) => {
     res.render('page/items/list', { 
       pageTitle: 'Item List Page',
