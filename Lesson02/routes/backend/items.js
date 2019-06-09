6b47c7336cf469efcce1d864a61cdd904545ecc5
huynvn010193 <hpn010193@gmail.com>
2019-06-09 12:59:48 +0700

    ZendVN-Done clip 3.2: Show Status filter

--- Lesson02/routes/backend/items.js
+++ Lesson02/routes/backend/items.js
@@ -4,10 +4,16 @@ var router = express.Router();
 const ItemModel = require('../../schemas/items');
 
 router.get('/', (req, res, next) => {
+  let statusFilter = [
+    {name: 'All', count: 1, link:'#', class: 'default'},
+    {name: 'Active', count: 2, link:'#', class: 'success'},
+    {name: 'InActive', count: 3, link:'#', class: 'warning'}
+  ];
   ItemModel.find({ }).then((items) => {
     res.render('page/items/list', { 
       pageTitle: 'Item List Page',
-      items: items
+      items: items,
+      statusFilter: statusFilter
     });
   });
   
