650d45b228b9ed6683a8cdcbe482a8f8d0eb5615
huynvn <hpn010193@gmail.com>
2020-02-13 07:47:53 +0700

    Pending clip

--- node-rest-shop/app.js
+++ node-rest-shop/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRouters = require('./api/routes/orders');
 
+// Routes which should handle requests
 app.use('/products',productRoutes);
 app.use('/orders',orderRouters);
 
