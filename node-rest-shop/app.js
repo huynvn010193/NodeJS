18ba9a0c96d446334ef436608453e3207a68ca11
Nguyễn Vũ Nhật Huy <v.huynvn@vinid.net>
2020-02-13 12:11:44 +0700

    Pending 02

--- node-rest-shop/app.js
+++ node-rest-shop/app.js
@@ -5,8 +5,26 @@ const morgan = require('morgan');
 const productRoutes = require('./api/routes/products');
 const orderRouters = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
 // Routes which should handle requests
 app.use('/products',productRoutes);
 app.use('/orders',orderRouters);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status(404);
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    console.log(error.status);
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;
