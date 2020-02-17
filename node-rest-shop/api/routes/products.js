cc802c1b90a00e46bd28b20f149117153ab30a56
Nguyễn Vũ Nhật Huy <v.huynvn@vinid.net>
2020-02-17 12:11:35 +0700

    Done validate API

--- node-rest-shop/api/routes/products.js
+++ node-rest-shop/api/routes/products.js
@@ -7,16 +7,31 @@ const mongoose = require('mongoose');
 
 router.get('/', (req, res, next) => {
     Product.find()
+    .select("name price _id")
     .exec()
     .then(docs =>  {
-        console.log(docs);
-        if(docs.length >= 0) {
-            res.status(200).json(docs);
-        } else {
-            res.status(404).json({
-                message: "No entries found"
+        const response = {
+            count: docs.length,
+            products: docs.map(doc => {
+                return {
+                    name: doc.name,
+                    price: doc.price,
+                    _id: doc._id,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:8080/products/' + doc._id
+                    }
+                }
             })
         }
+        res.status(200).json(response);
+        // if(docs.length >= 0) {
+        //     res.status(200).json(docs);
+        // } else {
+        //     res.status(404).json({
+        //         message: "No entries found"
+        //     })
+        // }
     })
     .catch(err => {
         console.log(err);
@@ -37,8 +52,16 @@ router.post('/', (req, res, next) => {
         .then(result => {
             console.log(result);
             res.status(200).json({
-                message: 'Handling POST request to /product',
-                createdProduct: result
+                message: 'Create product susscessfully',
+                createdProduct: {
+                    name: result.name,
+                    price: result.price,
+                    _id: result._id,
+                    request: {
+                        type: 'POST',
+                        url: 'http://localhost:8080/products/' + result._id
+                    }
+                }
             });
         })
         .catch(err => {
@@ -52,15 +75,21 @@ router.post('/', (req, res, next) => {
 
 router.get('/:productId',(req, res, next) => {
     const id = req.params.productId;
-    console.log("TCL: id", id);
     if(mongoose.Types.ObjectId.isValid(id)) {
         Product.findById(id)
+        .select('name price _id')
         .exec()
         .then(doc => {
             console.log(doc);
             if(doc) {
                 console.log("OK");
-                res.status(200).json(doc);
+                res.status(200).json({
+                    product: doc,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:8080/products'
+                    }
+                });
             } else {
                 console.log("fail");
                 res.status(404).json({
@@ -84,18 +113,21 @@ router.patch('/:productId', (req, res, next) => {
     const id = req.params.productId;
     const updateOps = {};
     for(const ops of req.body) {
-        console.log("ops",ops);
         updateOps[ops.propName] = ops.value;
-        console.log("updateOps",updateOps);
     }
-    console.log("TCL: updateOps", updateOps)
     Product.update({ _id: id }, { $set: updateOps })
     .exec()
     .then(result => {
-        console.log(result);
-        res.status(200).json({result})
+        res.status(200).json({
+            message: 'Product updated',
+            request: {
+                type: 'GET',
+                url: 'http://localhost:8080/products/' + id,
+            }
+        })
     })
     .catch(err => {
+        console.log("catch");
         res.status(500).json({ error: err })
     });
 });
@@ -107,7 +139,14 @@ router.delete("/:productId", (req, res, next) => {
     Product.remove({ _id: id })
     .exec()
     .then(result => {
-        res.status(200).json(result);
+        res.status(200).json({
+            message: 'Product deleted',
+            request: {
+                type: 'POST',
+                url: 'http://localhost:8080/products/' + id,
+                data: {name: "String", price:"Number"}
+            }
+        })
     })
     .catch(err => {
         console.log(err);
